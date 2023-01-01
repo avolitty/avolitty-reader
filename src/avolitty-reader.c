14600aa828a71c87d608055160297073208d6f87
William Parsons <avolitty@gmail.com>
2022-12-31 23:27:07 -0800

    Removing fseeko() because file stream is re-used.

--- src/avolitty-reader.c
+++ src/avolitty-reader.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
-#ifndef off_t
-#define off_t signed long int
-#endif
-
 void AvolittyReaderA(FILE **a, const char *b) {
 	*a = fopen(b, "rb");
 	return;
 }
 
 void AvolittyReaderB(FILE *a, signed long int *b, signed short int c, unsigned char *d, signed char *e) {
-	off_t g = (off_t) *b;
-	size_t h = (size_t) c;
-	size_t i = 1;
-	size_t j;
-	int k = 0;
-	signed long int l = 0L;
-	signed short int m = 0;
-	fseeko(a, g, k);
-	j = fread(d, i, h, a);
-	l = (signed long int) j;
-	*b = *b + l;
-	m = (signed short int) j;
+	size_t f = (size_t) c;
+	size_t g = 1;
+	size_t h;
+	signed long int i = 0L;
+	signed short int j = 0;
+	h = fread(d, g, f, a);
+	i = (signed long int) h;
+	*b = *b + i;
+	j = (signed short int) h;
 
-	if (c != m) {
+	if (c != j) {
 		*e = 1;
 	}
 
--- src/avolitty-reader.h
+++ src/avolitty-reader.h
@@ -2,6 +2,7 @@
 #define AVOLITTY_READER_H
 
 void AvolittyReaderA(FILE **a, const char *b);
+
 void AvolittyReaderB(FILE *a, signed long int *b, signed short int c, unsigned char *d, signed char *e);
 
 #endif
